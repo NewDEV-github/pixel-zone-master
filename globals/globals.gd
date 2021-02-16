6d9338e29d1c5d94364eea7fe38fffd9684f0ea3
JezSonic <karoltomaszewskimusic@gmail.com>
2021-02-16 09:50:49 +0100

    Add discord rich presence code - part 1

--- globals/globals.gd
+++ globals/globals.gd
@@ -7,25 +7,50 @@ var auto_load_mod = true
 var current_stage = ''
 var play_cutscenes
 var player_has_been_selected = false
+var activities: Discord.ActivityManager
+var users: Discord.UserManager
+var core: Discord.Core
+var os_rpc = ["Windows", "X11", "OSX"]
 signal collected_changed
 signal play_bgm(id, value)
-#func send_saved_data():
-#	print('SENDING SAVED DATA TO SilentWolf')
-#	var save = File.new()
-#	save.open_encrypted_with_pass("user://other_saves/0.save", File.READ, '3087283')
-#	var loaded_coins = save.get_line()
-#	debug_auth_player()
-#	var playername = str(SilentWolf.Auth.logged_in_player)
-#	if not str(playername) == '':
-#		SilentWolf.Scores.persist_score(playername, loaded_coins)
-#		SilentWolf.Scores.get_high_scores()
+
+func enum_to_string(the_enum: Dictionary, value: int) -> String:
+	var index: = the_enum.values().find(value)
+	var string: String = the_enum.keys()[index]
+	return string
+func _get_user_manager() -> Discord.UserManager:
+	var result = core.get_user_manager()
+	if result is int:
+		print(
+			"Failed to get user manager: ",
+			enum_to_string(Discord.Result, result)
+		)
+		return null
+	else:
+		return result
+
+
 func _set_collected(value):
 	collected = value
 	emit_signal("collected_changed")
 
 func _get_collected():
 	return collected
-
+func _get_image_manager() -> Discord.ImageManager:
+	var result = core.get_image_manager()
+	if result is int:
+		print(
+			"Failed to get image manager: ",
+			enum_to_string(Discord.Result, result)
+		)
+		return null
+	else:
+		return result
+func _process(_delta: float) -> void:
+	if core:
+		var result: int = core.run_callbacks()
+		if result != Discord.Result.OK:
+			print("Callbacks failed: ", enum_to_string(Discord.Result, result))
 func _send_data():
 #	google.submit_leaderboard(int(game_state.points), "CgkIisyP4OEeEAIQAQ")
 	debug_auth_player()
@@ -99,11 +124,46 @@ func _ready():
 	SilentWolf.configure_scores({
 	"open_scene_on_close": "res://scenes/Main Menu/GUI.tscn"
 	})
+	if os_rpc.has(OS.get_name()):
+		core = Discord.Core.new()
+		var result: int = core.create(
+			729429191489093702,
+			Discord.CreateFlags.DEFAULT
+		)
+		print("Created Discord Core: ", enum_to_string(Discord.Result, result))
+		if result != Discord.Result.OK:
+			core = null
+		else:
+			users = _get_user_manager()
+			users.connect("current_user_update", self, "_on_current_user_update")
+			activities = _get_activity_manager()
+			core.set_log_hook(Discord.LogLevel.DEBUG, self, "log_hook")
+			activities.register_command(str(OS.get_executable_path()))
+	else:
+		print("Can not start Discord Core")
 	scene_path = ''
 	var beta_2 = File.new()
 	beta_2.open(str(documents)+ "/Pixel Zone/.data/settings/advanced.save", File.READ)
 	var load_set = beta_2.get_line()
 	auto_load_mod = bool(str(load_set))
+func run_rpc(stage:String, character:String, is_in_menu=false):
+	if Discord.Core != null:
+		if os_rpc.has(OS.get_name()):
+			print("Starting RPC...")
+			var activity: = Discord.Activity.new()
+			if not is_in_menu:
+				activity.details = "Playing as %s" % [character]
+				activity.state = "At %s" % [stage]
+			elif is_in_menu:
+				activity.details = "At main menu"
+			activity.assets.large_image = "icon"
+
+			activity.timestamps.start = OS.get_unix_time()
+
+			activities.update_activity(activity, self, "update_activity_callback")
+	else:
+		print("Can not start Discord Core")
+
 func clear_logs():
 	var dir = Directory.new()
 	dir.open('user://')
@@ -111,3 +171,42 @@ func clear_logs():
 	dir.make_dir('logs')
 func debug_auth_player():
 	print(str(SilentWolf.Auth.logged_in_player))
+func update_activity_callback(result: int):
+	if result == Discord.Result.OK:
+		print("Updated Activity Successfully")
+	else:
+		print(
+			"Failed to update activity: ",
+			enum_to_string(Discord.Result, result)
+		)
+func _get_activity_manager() -> Discord.ActivityManager:
+	var result = core.get_activity_manager()
+	if result is int:
+		print(
+			"Failed to get activity manager: ",
+			enum_to_string(Discord.Result, result)
+		)
+		return null
+	else:
+		return result
+func log_hook(level: int, message: String) -> void:
+	print(
+		"[Discord SDK] ",
+		enum_to_string(Discord.LogLevel, level),
+		": ", message
+	)
+func get_current_user_callback(result: int, user: Discord.User) -> void:
+	if result != Discord.Result.OK:
+		print(
+			"Failed to get user: ",
+			enum_to_string(Discord.Result, result)
+		)
+	else:
+		print("Got Current User:")
+		print(user.username, "#", user.discriminator, "  ID: ", user.id)
+func _on_current_user_update() -> void:
+	users.get_current_user(self, "get_current_user_callback")
+	users.get_current_user_premium_type(
+		self, "get_current_user_premium_type_callback"
+	)
+
