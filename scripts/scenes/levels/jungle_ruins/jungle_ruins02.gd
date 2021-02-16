6a9570747be3d2cb71d6d0af3e652f7065a2ba5c
JezSonic <karoltomaszewskimusic@gmail.com>
2021-02-16 10:07:21 +0100

    Update jungle_ruins02.gd

--- scripts/scenes/levels/jungle_ruins/jungle_ruins02.gd
+++ scripts/scenes/levels/jungle_ruins/jungle_ruins02.gd
@@ -3,6 +3,7 @@ extends Node2D
 var scene
 var player_default = globals.selected_player
 func _ready():
+	globals.run_rpc("Jungle ruins 02")
 	add_child(Object(player_default))
 	globals._set_player_pos(0,0)
 	player_default.restart_position()
